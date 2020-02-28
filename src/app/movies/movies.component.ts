6ee2675917146b50aba6d9d1f7693f4e39771799
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-02-28 11:19:22 +0100

    Create movie service and movies component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.scss']
+})
+export class MoviesComponent implements OnInit {
+  constructor(private movieService: MovieService) { }
+  ngOnInit(): void { }
+}
